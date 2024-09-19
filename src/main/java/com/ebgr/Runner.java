86ba72220be04e0a331944145447b26604f9074a
ebgr <ebgr@cin.ufpe.br>
2024-09-19 05:07:47 -0300

    criados perfis de desenvolvimento e producao

--- src/main/java/com/ebgr/Runner.java
+++ src/main/java/com/ebgr/Runner.java
@@ -4,10 +4,12 @@ import com.ebgr.model.User;
 import com.ebgr.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("dev")
 public class Runner implements CommandLineRunner {
 
     @Autowired
@@ -17,6 +19,7 @@ public class Runner implements CommandLineRunner {
     PasswordEncoder encoder;
 
     @Override
+    @Profile("dev")
     public void run(String... args) throws Exception {
         User user1 = new User("user", "1234");
         User user2 = new User("usuario", "senha");
